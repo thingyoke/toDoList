cde3704543aaa3404412785affc9290cf1e895e9
thingyoke <thingyoke8955@gmail.com>
2025-03-12 15:46:25 +0800

    commit message

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Create New Task</title>
     <link rel="stylesheet" href="{{ asset('css/create.css') }}">
 </head>
+
 <body>
     <div class="container">
         <h1>Create New Task</h1>
-
         <form action="{{ route('tasks.store') }}" method="POST">
             @csrf
             <label for="title">Title</label>
@@ -15,9 +16,13 @@
 
             <label for="description">Description</label>
             <textarea name="description" id="description"></textarea>
+            <label for="description">Description</label>
+            <textarea name="description" id="description"></textarea>
 
             <button type="submit">Create</button>
         </form>
     </div>
+
 </body>
+
 </html>

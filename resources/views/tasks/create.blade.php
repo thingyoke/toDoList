b561d7e1cd5ab6e993ea4e1b621a7fdd3ca39640
thingyoke <thingyoke8955@gmail.com>
2025-03-13 10:06:00 +0800

    fix: remove unnecessary blank lines in task creation view

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -14,8 +14,6 @@
             <label for="title">Title</label>
             <input type="text" name="title" id="title" required>
 
-
-
             <label for="description">Description</label>
             <textarea name="description" id="description"></textarea>
             <button type="submit">Create</button>
